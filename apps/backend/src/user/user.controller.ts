40487affea30551bc1ac5feacc7c475b415a1817
MartaGut <martagutnsjjz@gmail.com>
2025-01-08 15:52:35 +0100

    11-any type removed

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from 'src/entities/user.entity';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
