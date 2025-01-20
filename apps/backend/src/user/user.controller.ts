1b0028076c51f6f3f318dff037ca52b8281ec700
MartaGut <martagutnsjjz@gmail.com>
2025-01-20 12:28:41 +0100

    13-dto for request and and response user made, migration fixed

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/entities/user.entity';
-
+import { CreateUserRequestDto } from './dto/create-user-request.dto';
+import { CreateUserResponseDto } from './dto/create-user-response.dto';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -11,11 +12,18 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Post()
-  create(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ): Promise<User> {
-    return this.userService.create(email, password);
+  @Post('create')
+  async createUser(
+    @Body() createUserRequestDto: CreateUserRequestDto,
+  ): Promise<CreateUserResponseDto> {
+    const user = await this.userService.create(
+      createUserRequestDto.email,
+      createUserRequestDto.password,
+    );
+
+    return {
+      id: user.id,
+      email: user.email,
+    };
   }
 }
