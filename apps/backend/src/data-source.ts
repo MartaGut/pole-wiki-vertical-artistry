73e76bb5b0a4b9a0557a0eaa36e832c369e30365
MartaGut <martagutnsjjz@gmail.com>
2025-02-10 11:26:38 +0100

    19-backend not working

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -3,10 +3,11 @@ import { PoleFigurine } from './entities/pole-figurine.entity';
 import { User } from './entities/user.entity';
 
 const AppDataSource = new DataSource({
+  migrationsRun: false,
   type: 'sqlite',
   database: 'database.sqlite',
   entities: [PoleFigurine, User],
-  migrations: ['./src/migrations/**.js'],
+  migrations: ['./src/migrations/**.ts'],
   synchronize: false,
 });
 
