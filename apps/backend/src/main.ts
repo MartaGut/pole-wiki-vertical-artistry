d35395fde0b889b7662c07285cace38c3f45dc8e
MartaGut <martagutnsjjz@gmail.com>
2025-02-14 10:01:28 +0100

    19 auth working, reddirection woriks

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,13 @@ import AppDataSource from './data-source';
 async function bootstrap() {
   await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
