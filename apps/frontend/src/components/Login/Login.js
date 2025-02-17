52e8dce25834774c24686a9381f6efb73d3e372e
MartaGut <martagutnsjjz@gmail.com>
2025-02-17 11:33:36 +0100

    19-js added to linter check

--- apps/frontend/src/components/Login/Login.js
+++ apps/frontend/src/components/Login/Login.js
@@ -3,61 +3,63 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState("");
-const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-const handleLogin = async (e) => {
-  e.preventDefault(); 
-  setError(""); 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError('');
 
-  try {
-    const response = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    try {
+      const response = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    if (response.ok) {
-      const data = await response.json(); 
-      localStorage.setItem("token", data.token);
-      navigate("/"); 
-    } else {
-      setError("Invalid username and password");
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        navigate('/');
+      } else {
+        setError('Invalid username and password');
+      }
+    } catch (error) {
+      setError('Something went wrong!');
     }
-  } catch (error) {
-    setError("Something went wrong!")
-  }
-}
+  };
 
-    return (
-      <div className='login-container'>
-        <h2 className="page-title">Login</h2>
-        {error && <p className='error-message'>{error}</p>}
-        <form className='form login-form' onSubmit={handleLogin}>
-          <label  className='label'>Username: </label>
-          <input
-          className='input'
+  return (
+    <div className="login-container">
+      <h2 className="page-title">Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form className="form login-form" onSubmit={handleLogin}>
+        <label className="label">Username: </label>
+        <input
+          className="input"
           type="text"
           value={username}
-          onChange={(e)=>setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
           required
-          />
-          <label className='label'>Password:</label>
-          <input
-              className='input'
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-          />
-          <button className='button' type="submit">Login</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Login;
+        />
+        <label className="label">Password:</label>
+        <input
+          className="input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button className="button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
